fe428876ed9129b27687b7d4bd3aecf61dad1945
Steven Ly <ly.steven16@gmail.com>
2021-07-22 01:25:18 -0500

    Adjustments to schema file/adding foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,24 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE departments (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE roles (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-  id INT PRIMARY KEY NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
